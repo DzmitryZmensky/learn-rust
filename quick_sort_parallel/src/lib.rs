e812f3f40e2216babfde040da0cc30b59e5e9c93
Dmitry V <dkevi@outlook.com>
2022-09-03 21:07:35 -0700

    make generic; rename project

--- quick_sort_parallel/src/lib.rs
+++ quick_sort_parallel/src/lib.rs
@@ -1,12 +1,12 @@
 use std::thread::{self};
 use num_cpus;
 
-pub fn quick_sort(slice: &mut [i32]) {
+pub fn quick_sort_parallel<T: Send + PartialOrd + Copy>(slice: &mut [T]) {
     let cores = num_cpus::get();
     quick_sort_recursive(slice, cores, 1);
 }
 
-fn quick_sort_recursive(slice: &mut [i32], cores: usize, level: u32) {
+fn quick_sort_recursive<T: Send + PartialOrd + Copy>(slice: &mut [T], cores: usize, level: u32) {
     if slice.len() <= 1 {
         return;
     }
@@ -27,7 +27,7 @@ fn quick_sort_recursive(slice: &mut [i32], cores: usize, level: u32) {
     }
 }
 
-fn partition(slice: &mut [i32]) -> (&mut [i32], &mut [i32]) {
+fn partition<T: PartialOrd + Copy>(slice: &mut [T]) -> (&mut [T], &mut [T]) {
     if slice.len() <=1 {
         panic!()
     }
@@ -59,7 +59,7 @@ mod tests {
     #[test]
     fn arbitrary_data() {
         let mut v = [5,3,6,4,2,1,7];
-        quick_sort(&mut v);
+        quick_sort_parallel(&mut v);
         let expected = [1,2,3,4,5,6,7];
         assert_eq!(expected, v);
     }
@@ -67,7 +67,7 @@ mod tests {
     #[test]
     fn already_sorted_data() {
         let mut v = [1,2,3,4,5,6,7];
-        quick_sort(&mut v);
+        quick_sort_parallel(&mut v);
         let expected = [1,2,3,4,5,6,7];
         assert_eq!(expected, v);
     }
@@ -75,7 +75,7 @@ mod tests {
     #[test]
     fn sorted_in_reverse_data() {
         let mut v = [7,6,5,4,3,2,1];
-        quick_sort(&mut v);
+        quick_sort_parallel(&mut v);
         let expected = [1,2,3,4,5,6,7];
         assert_eq!(expected, v);
     }
@@ -83,7 +83,7 @@ mod tests {
     #[test]
     fn all_duplicates() {
         let mut v = [1,1,1,1,1];
-        quick_sort(&mut v);
+        quick_sort_parallel(&mut v);
         let expected = [1,1,1,1,1];
         assert_eq!(expected, v);
     }
@@ -91,23 +91,23 @@ mod tests {
     #[test]
     fn some_duplicates() {
         let mut v = [3,1,2,3,2];
-        quick_sort(&mut v);
+        quick_sort_parallel(&mut v);
         let expected = [1,2,2,3,3];
         assert_eq!(expected, v);
     }
 
     #[test]
-    fn perf() {
+    fn perf_on_large_dataset() {
         let mut v = vec![0; 20 * 1000 * 1000];
         let min_value = i32::MIN;
         let max_value = i32::MAX;
         random_number::random_fill!(v, min_value..max_value);
          
-        quick_sort(&mut v); // it takes approximately 20 seconds on 4 core machine
+        quick_sort_parallel(&mut v); // it takes approximately 20 seconds on 4 core machine
         assert!(is_sorted(&v));
     }
 
-    fn is_sorted(v:&Vec<i32>) -> bool {
+    fn is_sorted<T: PartialOrd>(v:&Vec<T>) -> bool {
         for index in 1..v.len() {
             if v[index-1] > v[index] {
                 return false;
