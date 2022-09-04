0eb26c8994ebaed192ba466883e9dea35fb6f195
Dmitry V <dkevi@outlook.com>
2022-09-04 16:54:15 -0700

    make generic; rename tests

--- binary_search/src/lib.rs
+++ binary_search/src/lib.rs
@@ -1,16 +1,17 @@
-pub fn bsearch(v: &[i32], i:i32) -> Option<usize> {
-    let mut l:usize = 0;
-    let mut r:usize = v.len() - 1;
+pub fn bsearch<T: PartialOrd>(v: &[T], value:T) -> Option<usize> {
+    if v.len() == 0 { return None }
+    let mut l: usize = 0;
+    let mut r = v.len() - 1;
     while l < r {
         let m = l + (r - l) / 2;
-        if v[m] >= i {
+        if v[m] >= value {
             r = m;
         }
         else {
             l = m + 1;
         }
     }
-    if v[l] == i {
+    if v[l] == value {
         Some(l)
     } else {
         None
@@ -22,55 +23,63 @@ mod tests {
     use crate::bsearch;
 
     #[test]
-    fn test1() {
-        let v = [1,2,3];
+    fn empty_dataset() {
+        let v = [];
         let i = 2;
-        let result = bsearch(&v, i).unwrap();
-        assert_eq!(1, result);
+        let result = bsearch(&v, i);
+        assert_eq!(None, result);
     }
 
     #[test]
-    fn test2() {
+    fn middle_location() {
+        let v = [1,2,3];
+        let i = 2;
+        let result = bsearch(&v, i);
+        assert_eq!(Some(1), result);
+    }
+
+    #[test]
+    fn middle_location_duplicates() {
         let v = [1,2,2,3];
         let i = 2;
-        let result = bsearch(&v, i).unwrap();
-        assert_eq!(1, result);
+        let result = bsearch(&v, i);
+        assert_eq!(Some(1), result);
     }
 
     #[test]
-    fn test3() {
+    fn leftmost_location() {
         let v = [1,2,3];
         let i = 1;
-        let result = bsearch(&v, i).unwrap();
-        assert_eq!(0, result);
+        let result = bsearch(&v, i);
+        assert_eq!(Some(0), result);
     }
 
     #[test]
-    fn test4() {
+    fn leftmost_location_duplicates() {
         let v = [1,1,2,3];
         let i = 1;
-        let result = bsearch(&v, i).unwrap();
-        assert_eq!(0, result);
+        let result = bsearch(&v, i);
+        assert_eq!(Some(0), result);
     }
 
     #[test]
-    fn test5() {
+    fn rightmost_location() {
         let v = [1,2,3];
         let i = 3;
-        let result = bsearch(&v, i).unwrap();
-        assert_eq!(2, result);
+        let result = bsearch(&v, i);
+        assert_eq!(Some(2), result);
     }
 
     #[test]
-    fn test6() {
+    fn rightmost_location_duplicates() {
         let v = [1,2,3,3];
         let i = 3;
-        let result = bsearch(&v, i).unwrap();
-        assert_eq!(2, result);
+        let result = bsearch(&v, i);
+        assert_eq!(Some(2), result);
     }
 
     #[test]
-    fn test7() {
+    fn missing_in_the_middle() {
         let v = [1,3];
         let i = 2;
         let result = bsearch(&v, i);
@@ -78,7 +87,7 @@ mod tests {
     }
 
     #[test]
-    fn test8() {
+    fn missing_greater_value() {
         let v = [1,3];
         let i = 4;
         let result = bsearch(&v, i);
@@ -86,7 +95,7 @@ mod tests {
     }
 
     #[test]
-    fn test9() {
+    fn missing_smaller_value() {
         let v = [1,3];
         let i = 0;
         let result = bsearch(&v, i);
