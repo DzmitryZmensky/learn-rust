aa3646f3cd993b39ef6ae0c3ce5f67686296e3ca
Dmitry V <dkevi@outlook.com>
2022-09-03 17:11:22 -0700

    implement parallel sort

--- quick_sort/src/main.rs
+++ quick_sort/src/main.rs
@@ -1,3 +1,6 @@
+use std::thread::{self};
+use num_cpus;
+
 fn main() {
     let mut v = [5,3,6,4,2,1,7];
     println!("{:?}", &v);
@@ -6,17 +9,32 @@ fn main() {
 }
 
 fn quick_sort(slice: &mut [i32]) {
+    let cores = num_cpus::get();
+    quick_sort_recursive(slice, cores, 1);
+}
+
+fn quick_sort_recursive(slice: &mut [i32], cores: usize, level: u32) {
     if slice.len() <= 1 {
         return;
     }
 
     let (left, right) = partition(slice);
-    quick_sort(left);
-    quick_sort(right);
+    if level < 10 && 2_u32.pow(level) as usize <= cores {
+        thread::scope(|scope| {
+            let join_handle = scope.spawn(||{
+                quick_sort_recursive(right, cores, level + 1);
+            });
+
+            quick_sort_recursive(left, cores, level + 1);
+            join_handle.join().unwrap();
+        });
+    } else {
+        quick_sort_recursive(left, cores, level + 1);
+        quick_sort_recursive(right, cores, level + 1);
+    }
 }
 
 fn partition(slice: &mut [i32]) -> (&mut [i32], &mut [i32]) {
-    println!("partitioning: {:?}", &slice);
     if slice.len() <=1 {
         panic!()
     }
@@ -39,7 +57,6 @@ fn partition(slice: &mut [i32]) -> (&mut [i32], &mut [i32]) {
 
     let (l, r) = slice.split_at_mut(write_index);
     return (l, &mut r[1..]);
-    //(&mut slice[..write_index], &mut slice[write_index + 1 ..]);
 }
 
 #[cfg(test)]
@@ -47,10 +64,47 @@ mod tests {
     use super::*;
 
     #[test]
-    fn internal() {
+    fn test_data_in_reverse_order() {
         let mut v = [5,3,6,4,2,1,7];
         quick_sort(&mut v);
         let expected = [1,2,3,4,5,6,7];
         assert_eq!(expected, v);
+    }
+
+    #[test]
+    fn test_all_duplicates() {
+        let mut v = [1,1,1,1,1];
+        quick_sort(&mut v);
+        let expected = [1,1,1,1,1];
+        assert_eq!(expected, v);
+    }
+
+    #[test]
+    fn test_some_duplicates() {
+        let mut v = [3,1,2,3,2];
+        quick_sort(&mut v);
+        let expected = [1,2,2,3,3];
+        assert_eq!(expected, v);
+    }
+
+    #[test]
+    fn test_perf() {
+        let mut v = vec![0; 20 * 1000 * 1000];
+        let min_value = i32::MIN;
+        let max_value = i32::MAX;
+        random_number::random_fill!(v, min_value..max_value);
+         
+        quick_sort(&mut v); // it takes approximately 20 seconds on 4 core machine
+        assert!(is_sorted(&v));
+    }
+
+    fn is_sorted(v:&Vec<i32>) -> bool {
+        for index in 1..v.len() {
+            if v[index-1] > v[index] {
+                return false;
+            }
+        };
+
+        true
     }
 }
